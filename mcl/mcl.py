c7b424671eacfef462eab011a0425dc365a5e9eb
Krzysztof Nowak <nowak196@gmail.com>
2020-01-26 04:40:39 +0100

    Added mcl initializaton (#2)

---
+++ mcl/mcl.py
@@ -0,0 +1,22 @@
+import ctypes
+from .curve_type import CurveType
+
+
+MCLBN_FR_UNIT_SIZE = 4
+MCLBN_FP_UNIT_SIZE = 6
+
+FR_SIZE = MCLBN_FR_UNIT_SIZE
+G1_SIZE = MCLBN_FP_UNIT_SIZE * 3
+G2_SIZE = MCLBN_FP_UNIT_SIZE * 6
+GT_SIZE = MCLBN_FP_UNIT_SIZE * 12
+MCLBN_COMPILED_TIME_VAR = (MCLBN_FR_UNIT_SIZE * 10) + MCLBN_FP_UNIT_SIZE
+
+mcl = None
+
+def mcl_init(curve_type: CurveType):
+    global mcl
+    mcl = ctypes.CDLL("libmcl.so", mode = ctypes.RTLD_GLOBAL)
+    mcl = ctypes.CDLL("libmclbn384_256.so")
+    ret = mcl.mclBn_init(curve_type, MCLBN_COMPILED_TIME_VAR)
+    if ret:
+        raise RuntimeError(f'Init error: {ret}')
